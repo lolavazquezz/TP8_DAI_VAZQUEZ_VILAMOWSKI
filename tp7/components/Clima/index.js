d5cac4705ea868d2211fa82f6ae471f26657596a
LOLA VAZQUEZ <47436085@ort>
2024-09-06 10:28:51 -0300

    api clima

--- tp7/components/Clima/index.js
+++ tp7/components/Clima/index.js
@@ -9,24 +9,22 @@ export default Clima = () => {
     const [consultar, setConsultar] = useState(true);
     const { ciudad, pais, temperatura, humedad, descripcion, error,fondo, icono } = clima;
     useEffect(() => {
-        const consultarAPI = async () => {
+        const fetchClima = async () => {
 
             if (consultar) {
                 const appId = '572439d21bda87f48f83dc74b1793e24';
                 const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}&units=metric`;
-
-                const respuesta = await axios.get(url);
-
+                const response = await axios.get(url);
                 setClima({
-                    ciudad: respuesta.data.name,
-                    pais: respuesta.data.sys.country,
-                    temperatura: respuesta.data.main.temp,
-                    humedad: respuesta.data.main.humidity,
-                    descripcion: respuesta.data.weather[0].description,
+                    ciudad: response.data.name,
+                    pais: response.data.sys.country,
+                    temperatura: response.data.main.temp,
+                    humedad: response.data.main.humidity,
+                    descripcion: response.data.weather[0].description,
                     error: false
                 })
             }
         }
-        consultarAPI();
+        fetchClima();
     }, [consultar, ciudad, pais]);
 }
